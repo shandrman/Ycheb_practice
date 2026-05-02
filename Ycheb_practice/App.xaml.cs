3e4d150aa5dd40e57cb62ee09c7da0f569b8f491
shandrman <NikitaKarpenko83@gmail.com>
2026-05-02 23:26:05 +0700

    Добавьте файлы проекта.

---
+++ Ycheb_practice/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ycheb_practice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
